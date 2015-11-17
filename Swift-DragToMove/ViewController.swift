5e4cf368671760fbc1d68b7683ba300a9b8beb71
Yin Kokpheng <yinkokpheng@yahoo.com>
2015-11-17 16:27:27 +0700

    Initial Commit

---
+++ Swift-DragToMove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-DragToMove
+//
+//  Created by Yin Kokpheng on 11/17/15.
+//  Copyright © 2015 Yin Kokpheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
